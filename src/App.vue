8ba20e8735aa36b132d1ffb0d83298632738d8ec
robertvlad <vladrob96@gmail.com>
2023-03-12 16:55:39 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+import PostMain from './components/PostMain.vue';
+export default {
+  components: {
+    PostMain
+  }
+}
+
+</script>
+
+
+<template>
+
+<PostMain></PostMain>
+
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/PostMain.vue
@@ -0,0 +1,49 @@
+<script>
+
+export default {
+    name: 'PostMain',
+    data() {
+        return {
+            posts: [],
+            loading: true,
+            baseUrl: 'http://127.0.0.1:8000'
+        }
+    },
+    methods: {
+        getPosts() {
+            this.loading = true;
+            axios.get(`${this.baseUrl}/api/posts`).then((response) => {
+                if (response.data.success) {
+                    this.post = response.data.posts
+                    this.loading = false;
+                }
+                else {
+
+                }
+            })
+        }
+    },
+    mounted() {
+        this.getPosts()
+    },
+}
+
+</script>
+
+
+<template lang="">
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+
+        </div>
+    </div>
+</div>
+
+</template>
+
+
+<style lang="scss" scoped>
+    
+</style>
